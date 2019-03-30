21a9cbe43f504395943c9ad222610b7999f7b2e4
yulisaambella <49061443+yulisaambella@users.noreply.github.com>
2019-03-30 18:43:36 +0700

    yulisa ambella

---
+++ TUGAS.php
@@ -0,0 +1,4 @@
+<?php
+	$list=explode(" ","1 2 3 4");
+	echo "$list[3]	$list[2] $list[1] $list[0]";
+?>
---
+++ tugass2.php
@@ -0,0 +1,6 @@
+<?php
+$angka = array("1","2","3","4");
+rsort($angka);
+$angka2 = implode(" ",$angka);
+echo $angka2;
+?>
